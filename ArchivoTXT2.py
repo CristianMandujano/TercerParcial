659cfeb21b2490bb4290760769155aabeb052c6b
CristianMandujano <72647leo@universidaddeleon.edu.mx>
2026-03-22 21:28:11 -0600

    Traductor

---
+++ ArchivoTXT2.py
@@ -0,0 +1,115 @@
+import os
+import os
+ 
+#***** Crear archivo *****
+def crear_archivo():
+    nombre = input("Nombre del Archivo: ")
+    with open(nombre, "w") as archivo: #Mande llamar el archivo cuando abre el archivo y dejas de usarlo lo cierra, with cierra el archivo para quitar el espacio en la ram  
+        print ("Archivo creado correctamente")
+       
+ 
+
+#***** agregar datos al archivo *****
+def escribir_archivo():
+    nombre = input("Nombre del archivo: ")
+    texto = input("Escribe el texto a guardar: ")
+   
+    with open(nombre, "w") as archivo:
+        archivo.write(texto)
+       
+    print("Texto guardado correctamente")
+   
+ 
+#***** Escribir Archivo *****
+def agregar_texto():
+    nombre = input("Nombre del archivo: ")
+    texto = input("Texto a agregar: ")
+   
+    with open(nombre, "a") as archivo:
+        archivo.write("\n" + texto)
+       
+    print("Texto agregado correctamente")
+
+
+#***** Leer datos del archivo *****
+def leer_texto():
+    #nombre = input("Nombre del Archivo")
+    nombre = "prueba1.txt"
+   
+    try:
+        with open(nombre, "r") as archivo:
+            contenido = archivo.read()
+            os.system("cls")
+            print("\nContenido del Archivo: ")
+            print(contenido)
+            print("-------------------------")
+            archivo.seek(0) #Donde posicionar para empezar a leer
+            contenido = archivo.read()
+            print(contenido)
+    except FileNotFoundError:
+        print("El archivo no existe")
+       
+ 
+def leer_archivo():
+    #nombre = input("Nombre del Archivo")
+    nombre = "prueba1.txt"
+   
+    try:
+        with open(nombre, "r") as archivo:
+            contenido = archivo.readlines()
+            os.system("cls")
+            print("\nContenido del Archivo: ")
+            print(contenido)
+           
+            for linea in contenido:
+                print(linea.strip())
+               
+    except FileNotFoundError:
+        print("El archivo no existe")
+       
+ 
+def buscar_palabra():
+    nombre = input("Nombre del archivo: ")
+    palabra = input("Palabra a buscar: ")
+   
+    try:
+        with open(nombre, "r") as archivo:
+            contenido = archivo.read()
+           
+        if palabra in contenido:
+            print("La palara fue encontrada: ")
+        else:
+            print("La palabra no se encuentra en el archivo")
+    except FileExistsError:
+        print("El archivo no existe")
+       
+ 
+def menu():
+    while True:
+        print("\n--- GESTOR DE ARCHIVOS ---")
+        print("1. Crear Archivo")
+        print("2. Escribir en Archivo")
+        print("3. Agregar Texto")
+        print("4. Leer Archivo")
+        print("5. Buscar Palabra")
+        print("6. Salir")
+       
+        opcion = input("Seleccione una opcion: ")
+       
+        if opcion == "1":
+            crear_archivo()
+        elif opcion == "2":
+            escribir_archivo()
+        elif opcion == "3":
+            agregar_texto()
+        elif opcion == "4":
+            leer_archivo()
+        elif opcion == "5":
+            buscar_palabra()
+        elif opcion == "6":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opcion Invalida")
+           
+menu()
---
+++ Traductor.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+from tkinter import messagebox
+
+ARCHIVO = "diccionario.txt"
+
+def registrar_palabra():
+    ing = entrada_ing.get().lower().strip()
+    esp = entrada_esp.get().lower().strip()
+    
+    if not ing or not esp:
+        messagebox.showwarning("Error", "Llena ambos campos")
+        return
+
+    with open(ARCHIVO, "a", encoding="utf-8") as f:
+        f.write(f"{ing}:{esp}\n")
+    
+    messagebox.showinfo("Éxito", "Palabra guardada")
+    entrada_ing.delete(0, tk.END)
+    entrada_esp.delete(0, tk.END)
+
+def buscar_traduccion(Palabra):
+    buscar = entrada_busqueda.get().lower().strip()
+    
+    try:
+        with open(ARCHIVO, "r", encoding="utf-8") as f:
+            lineas = f.readlines() 
+            
+        encontrado = False
+        for linea in lineas:
+            datos = linea.strip().split(":")
+            if len(datos) == 2:
+                ingles, espanol = datos
+                
+                if Palabra == "en_es" and ingles == buscar:
+                    lbl_resultado.config(text=f"Español: {espanol}", fg="blue")
+                    encontrado = True
+                    break
+                elif Palabra == "es_en" and espanol == buscar:
+                    lbl_resultado.config(text=f"Inglés: {ingles}", fg="green")
+                    encontrado = True
+                    break
+        
+        if not encontrado:
+            lbl_resultado.config(text="No encontrada", fg="red" )
+            
+    except FileNotFoundError:
+        messagebox.showerror("Error", "Registra una palabra primero.")
+
+ventana = tk.Tk()
+ventana.title("Traductor")
+ventana.geometry("400x450")
+
+tk.Label(ventana, text="Palabra a buscar", font=("Arial", 12, "bold")).pack(pady=10)
+entrada_busqueda = tk.Entry(ventana, width=30)
+entrada_busqueda.pack()
+
+tk.Label(ventana, text="Traducción: ", font=("Arial", 12, "bold")).pack(pady=10)
+
+f_btns = tk.Frame(ventana)
+f_btns.pack(pady=10)
+tk.Button(f_btns, text="Ingles-Español", command=lambda: buscar_traduccion("en_es")).grid(row=0, column=0, padx=5)
+tk.Button(f_btns, text="Español-Ingles", command=lambda: buscar_traduccion("es_en")).grid(row=0, column=1, padx=5)
+
+lbl_resultado = tk.Label(ventana, text="", font=("Arial", 12))
+lbl_resultado.pack(pady=10)
+
+tk.Label(ventana, text="Registra una nueva palabra", font=("Arial", 12, "bold")).pack(pady=10)
+tk.Label(ventana, text="Español:").pack()
+entrada_esp = tk.Entry(ventana)
+entrada_esp.pack()
+tk.Label(ventana, text="Inglés:").pack()
+entrada_ing = tk.Entry(ventana)
+entrada_ing.pack()
+
+tk.Button(ventana, text="Registrar", command=registrar_palabra).pack(pady=20)
+
+ventana.mainloop()
